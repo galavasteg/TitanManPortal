b3fd2baeaea373503efe70f2bf1286309c917644
Artem Shakurov <galavasteg@gmail.com>
2020-04-09 12:28:48 +0300

    apps admins

--- titanmanportal/apps/moderation/admin.py
+++ titanmanportal/apps/moderation/admin.py
@@ -1,8 +1,40 @@
+from django import forms
 from django.contrib import admin
 
+from periods.services import PeriodService
 from .models import Moderation
+from .services import ModerationService
+
+
+class Members2ModerateInlineAdmin(admin.TabularInline):
+    model = Moderation.members.through
+    fields = ('member',)
+    extra = 1
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "member":
+
+            kwargs["queryset"] = ModerationService.get_queryset()
+
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class ModerationAdminForm(forms.ModelForm):
+
+    class Meta:
+        fields = ('period', 'moderator')
+        model = Moderation
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # request = args[0]
+        # self.initial['moderator'] = request.user
+        self.initial['period'] = PeriodService.get_current_period()
 
 
 @admin.register(Moderation)
 class ModerationAdmin(admin.ModelAdmin):
-    ...
+    form = ModerationAdminForm
+
+    readonly_fields = form._meta.fields
+    inlines = (Members2ModerateInlineAdmin,)
--- titanmanportal/apps/users/admin.py
+++ titanmanportal/apps/users/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from django.contrib.admin import sites
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    ...
+    # fieldsets = (
+    #     (
+    #         None,
+    #         {
+    #             "fields": (
+    #                 "email",
+    #                 "phone_number",
+    #             ),
+    #         },
+    #     ),
+    #     (
+    #         _("Personal info"),
+    #         {
+    #             "fields": (
+    #                 "first_name",
+    #                 "last_name",
+    #                 "photo",
+    #             ),
+    #         },
+    #     ),
+    #     (
+    #         _("Permissions"),
+    #         {
+    #             "fields": (
+    #                 "is_active",
+    #                 "groups",
+    #             ),
+    #         },
+    #     ),
+    #     (
+    #         _("Important dates"),
+    #         {
+    #             "fields": (
+    #                 "last_login",
+    #                 "date_joined",
+    #             ),
+    #         },
+    #     ),
+    # )
+    #
+    # list_display = ("email", "first_name", "last_name", )
+
