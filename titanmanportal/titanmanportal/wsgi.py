4e7b14469119343f82fe6c2b2aafaf3bcc6dc3d4
Artem Shakurov <galavasteg@gmail.com>
2020-04-17 14:50:32 +0300

    wsgi: path extend

--- titanmanportal/titanmanportal/wsgi.py
+++ titanmanportal/titanmanportal/wsgi.py
@@ -8,8 +8,16 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+
+
+this_file = Path(__file__).absolute()
+# target is ".../TitanManPortal/titanmanportal/"
+PRG_DIR = this_file.parent.parent
+sys.path.insert(0, str(PRG_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'titanmanportal.settings')
 
