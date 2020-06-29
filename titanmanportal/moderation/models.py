5a6e0f495f9bee3732b09171e4a15b7a851982f9
Artem Shakurov <galavasteg@gmail.com>
2020-06-29 21:42:21 +0300

    moderation models refactoring

--- titanmanportal/moderation/models.py
+++ titanmanportal/moderation/models.py
@@ -1,32 +1,15 @@
 from django.db import models
 from django.db.models import PROTECT
 from django.utils import timezone
-from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 from django_fsm import FSMField, transition
 
+from _utils.models import HistoryModel
 from periods.models import Period
+from users.models import User
 
 
-User = get_user_model()
-
-
-class BaseModel(models.Model):
-    modified_datetime = models.DateTimeField(editable=False)
-
-    def save(self, *args, **kwargs):
-        self.modified_datetime = timezone.now()
-        return super(BaseModel, self).save(*args, **kwargs)
-
-    @staticmethod
-    def get_fields():
-        return 'modified_datetime',
-
-    class Meta:
-        abstract = True
-
-
-class Moderation(BaseModel):
+class Moderation(HistoryModel):
 
     period = models.ForeignKey(
         Period,
