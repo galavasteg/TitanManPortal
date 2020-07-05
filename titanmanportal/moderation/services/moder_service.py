14f2da2fa2921e8d7842689738ccbd9261c541c9
Artem Shakurov <galavasteg@gmail.com>
2020-07-05 17:53:43 +0300

    moderation service: rename, yagni

--- titanmanportal/moderation/services/moder_service.py
+++ titanmanportal/moderation/services/moder_service.py
@@ -9,23 +9,24 @@ from moderation.models import Moderation
 class ModerationService:
 
     @classmethod
-    def get_queryset(cls, exclude_users: Tuple[User] = ()
-                     ) -> User.objects:
+    def get_user_qs(cls, exclude_users: Tuple[User] = ()
+                    ) -> User.objects:
         qs = User.objects.filter()
         return qs
 
     @classmethod
-    def add_member_to_moder(cls, moder: User,
-                            member: User,
-                            period: PeriodService.PERIOD_MODEL):
-        err_msg = ''
-        if moder is not member:
-            err_msg = _('Moderator cannot moderates himself')
-        # TODO: check league?
-
-        if err_msg:
-            raise AttributeError(err_msg)
-
-        members = moder.moderates.filter(period=period).all()
-        members.add(member)
-        moder.save()
+    # @classmethod
+    # def add_member_to_moder(cls, moder: User,
+    #                         member: User,
+    #                         period: PeriodService.PERIOD_MODEL):
+    #     err_msg = ''
+    #     if moder is not member:
+    #         err_msg = _('Moderator cannot moderates himself')
+    #     # TODO: check league?
+    #
+    #     if err_msg:
+    #         raise AttributeError(err_msg)
+    #
+    #     members = moder.moderates.filter(period=period).all()
+    #     members.add(member)
+    #     moder.save()
